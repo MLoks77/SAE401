344029aa6aae96fe685481c5920eac27e190f6ac
Maxime <maximederenes@gmail.com>
2026-02-17 08:52:47 +0100

    Push router

--- FRONT/SAE401/src/App.jsx
+++ FRONT/SAE401/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Analyse from './pages/Analyse';
+import Carte from './pages/carte';
+import GraphCreation from './pages/graphCreation';
+import GridCompar from './pages/gridCompar';
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Analyse />} />
+          <Route path="/carte" element={<Carte />} />
+          <Route path="/gridCompar" element={<GridCompar />} />
+          <Route path="/graphCreation" element={<GraphCreation />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
