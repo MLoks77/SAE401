25eaa984e7847f85359d8a2db23fd15af214a982
Alexouuuu <alexchisiu@gmail.com>
2026-02-19 08:55:57 +0100

    fix bug footer

--- FRONT/SAE401/src/components/footer.jsx
+++ FRONT/SAE401/src/components/footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer>
-            <div className="flex justify-center items-center fixed bottom-0 w-full h-10 bg-[#133379] text-white">
+            <div className="flex justify-center items-center fixed bottom-0 w-full h-12 bg-[#133379] text-white">
                 <p className='text-lg font-bold'>© {new Date().getFullYear()} SAE401 </p>
             </div>
         </footer>
