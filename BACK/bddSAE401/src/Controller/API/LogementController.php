c710a12f0d7b16dcc9a398462ff2bae84dfbdf00
Alexouuuu <alexchisiu@gmail.com>
2026-03-06 16:18:47 +0100

    ajout api logements

--- BACK/bddSAE401/src/Controller/API/LogementController.php
+++ BACK/bddSAE401/src/Controller/API/LogementController.php
@@ -1,5 +1,6 @@
 <?php
 
+// Chisiu Sebastien
 // TD3 le :  https://docs.google.com/document/d/1cmGmHt2lEVTiunOXm3K0k81q1FPe75wV_yVk5F02mB4/edit?tab=t.0#heading=h.m0928a1c55c0
 
 namespace App\Controller\API;
@@ -7,14 +8,42 @@ namespace App\Controller\API;
 use App\Repository\LogementsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LogementController extends AbstractController
 {
     #[Route('/api/logements', methods: ['GET'], name: 'api_logements_get')]
-    public function index(LogementsRepository $repository): JsonResponse
+    public function index(LogementsRepository $repository, Request $request): JsonResponse
     {
-        return $this->json($repository->findAll());
+        $id_logement = $request->query->get('id_logement');
+        $annee = $request->query->get('annee');
+        $code_dept = $request->query->get('code_dept');
+        $nb_logements = $request->query->get('nb_logements');
+        $taux_logements_sociaux = $request->query->get('taux_logements_sociaux');
+        $taux_logements_vacants = $request->query->get('taux_logements_vacants');
+
+        $logementdatas = [];
+        if ($id_logement) {
+            $logementdatas['id'] = $id_logement;
+        }
+        if ($annee) {
+            $logementdatas['annee'] = $annee;
+        }
+        if ($code_dept) {
+            $logementdatas['code_dept'] = $code_dept;
+        }
+        if ($nb_logements) {
+            $logementdatas['nb_logements'] = $nb_logements;
+        }
+        if ($taux_logements_sociaux) {
+            $logementdatas['taux_logements_sociaux'] = $taux_logements_sociaux;
+        }
+        if ($taux_logements_vacants) {
+            $logementdatas['taux_logements_vacants'] = $taux_logements_vacants;
+        }
+
+        $logements = $repository->findBy($logementdatas);
+        return $this->json($logements, 200, [], ['groups' => 'logement:read']);
     }
-    // il récupère tout, à vous de mettre ce dont vous avez besoin
 }
