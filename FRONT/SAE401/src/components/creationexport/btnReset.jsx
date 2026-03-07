6e42dd438cdfa1a8641f3ad35fba20ef39aa27a0
Maxime <maximederenes@gmail.com>
2026-03-07 23:39:04 +0100

    Fix

--- FRONT/SAE401/src/components/creationexport/btnReset.jsx
+++ FRONT/SAE401/src/components/creationexport/btnReset.jsx
@@ -2,7 +2,7 @@
 const BtnReset = ({ reset }) => {
     return (
         <div className="p-4 bg-[#1A2432]">
-            <button onClick={reset} className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-3 rounded-lg transition-colors cursor-pointer active:scale-[0.98]">
+            <button onClick={reset} className="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-2 rounded-lg transition-colors cursor-pointer active:scale-[0.98]">
                 Réinitialiser
             </button>
         </div>
